7b5d339a274a2f98e8a7f5572448deaf741abaf3
Mihir Sharma <mihir.sharma@unc.edu>
2025-03-06 14:52:53 -0500

    extende access token expiration minutes to 3000 lmao hope we don't get hacked

--- app/services/auth.py
+++ app/services/auth.py
@@ -16,7 +16,7 @@ from app.schemas.auth import Token, TokenData
 
 SECRET_KEY = settings.JWT_SECRET_KEY
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 3000
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
