daf9351aa40eb2c6f29cb4c68bb29162955b7682
Luke Briden <lukebriden2023@gmail.com>
2025-04-14 14:24:54 -0400

    [lukeb23] - Finished logger

---
+++ app/logger.py
@@ -0,0 +1,16 @@
+import logging
+"""Logger for logging errors."""
+
+consolehandle = logging.StreamHandler()
+consolehandle.setLevel(logging.INFO)
+
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
+consolehandle.setFormatter(formatter)
+
+# Returns the logger based on which file its in
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        logger.addHandler(consolehandle)
+    return logger
